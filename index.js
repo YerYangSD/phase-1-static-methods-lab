15b035c66b2d452b2c635c3147e7482dfb53f12f
Yer Yang <yyhmg_86@yahoo.com>
2023-06-07 23:29:58 -0700

    Creates Test Cases

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ class Formatter {
     // takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always captalizes the first word
     // 
     return string.split(" ").map(word =>{
-      const excludeWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+      const excludeWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from", "in"]
       return excludeWords.includes(word) ? [word] : word[0].toUpperCase() + word.slice(1)
     }).join(" ")
   }
 }
+console.log("Expect: Letter")
 console.log(Formatter.capitalize("letter"))
+
+console.log("Expect: the cat in the hat")
 console.log(Formatter.sanitize("the #cat $in &the =hat"))
-console.log(Formatter.titleize("charlie an the chocolate factory"))
+
+console.log("Expect: Charlie and the Chocolate Factory")
+console.log(Formatter.titleize("charlie and the chocolate factory"))
+
+console.log("Expect: The Cat in the Hat")
 console.log(Formatter.titleize("the cat in the hat"))

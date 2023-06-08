5fbdb195c5570d6ddfeb713e622a0e9b2092b58d
Yer Yang <yyhmg_86@yahoo.com>
2023-06-07 20:19:17 -0700

    Creates Two Static Methods: One Capitalizes And The Other Sanitizes A String

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    //takes in a string and capitalizes the first letter
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    //takes in a string and removes all non-alphanumeric characters except for dashes, single quotes and spaces.
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "")
+  }
+  //static titleize() {
+  //takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always captalizes the first word
+  //}
 }

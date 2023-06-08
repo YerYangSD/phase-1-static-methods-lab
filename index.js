2a849a50365868ffebc39bdb0fbc41326b6a321f
Yer Yang <yyhmg_86@yahoo.com>
2023-06-07 22:05:49 -0700

    Creates A Static Method That Capitalizes Every Word In The Title

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ class Formatter {
     //takes in a string and removes all non-alphanumeric characters except for dashes, single quotes and spaces.
     return string.replace(/[^A-Za-z0-9-'\s]+/g, "")
   }
-  //static titleize() {
-  //takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always captalizes the first word
-  //}
+  static titleize(string) {
+    // takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always captalizes the first word
+    // 
+    return string.split(" ").map(word =>{
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(" ")
+  }
 }
+console.log(Formatter.capitalize("letter"))
+console.log(Formatter.sanitize("the #cat $in &the =hat"))
+console.log(Formatter.titleize("the cat in the hat"))
